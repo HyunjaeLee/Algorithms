7dd2e51fa461aef9b70723adf4c2aa9d3d0937a3
Hyunjae Lee <leeh18@g.skku.edu>
2022-12-13 07:32:06 +0000

    Add mobius function (Closes #5)

---
+++ math/mobius.hpp
@@ -0,0 +1,35 @@
+#ifndef MOBIUS_HPP
+#define MOBIUS_HPP
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+std::vector<int> mobius(int n) {
+    assert(n > 0);
+    std::vector<int> prime, mu(n + 1);
+    std::vector<char> is_prime(n + 1);
+    std::fill(is_prime.begin() + 2, is_prime.end(), true);
+    mu[1] = 1;
+    for (auto i = 2; i <= n; ++i) {
+        if (is_prime[i]) {
+            prime.push_back(i);
+            mu[i] = -1;
+        }
+        for (auto p : prime) {
+            if (i * p > n) {
+                break;
+            }
+            is_prime[i * p] = false;
+            if (i % p == 0) {
+                mu[i * p] = 0;
+                break;
+            } else {
+                mu[i * p] = -mu[i];
+            }
+        }
+    }
+    return mu;
+}
+
+#endif // MOBIUS_HPP
---
+++ math/mobius.md
@@ -0,0 +1,7 @@
+---
+title: Möbius function
+documentation_of: ./mobius.hpp
+---
+
+### Complexity
++ $\mathcal{O}\left(n\right)$
